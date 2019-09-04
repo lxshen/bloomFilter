4df2b465b2157b392221758703601471bce92c31
李广辉 <liguanghui@tuiquan.com>
2019-09-04 16:25:24 +0800

    pypi

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:   # 这里就是读取readme文件的内容了。
+with open("README.md", "r", encoding='utf-8') as fh:   # 这里就是读取readme文件的内容了。
     long_description = fh.read()
 
 
 setup(
-    name='multiprocessing_log_manager',  # '库的名称,一般写成文件夹的名字就可以了，也有的人不写成文件夹的名字，那么pip install和具体的import使用时候就不一样了，用起来会十分蛋疼，有一些包就是这样的。比如一个包，安装时候是pip install  xxx,但当你要使用时候要import yyy
+    name='lx_bloomfilter',  # '库的名称,一般写成文件夹的名字就可以了，也有的人不写成文件夹的名字，那么pip install和具体的import使用时候就不一样了，用起来会十分蛋疼，有一些包就是这样的。比如一个包，安装时候是pip install  xxx,但当你要使用时候要import yyy
 
 
     version="0.1.0",                  # 版本，每次发版本都不能重复，每次发版必须改这个地方
@@ -15,12 +15,12 @@ setup(
         'Python implementation Bloom filter'   # 一个简介，别人搜索包时候，这个概要信息会显示在在搜索列表中
 
     ),
-    long_description=open('README.md', 'r').read(),    # 这是详细的，一般是交别人怎么用，很多包没写，那么在官网就造成没有使用介绍了
+    long_description=open('README.md', 'r', encoding='utf-8').read(),
     long_description_content_type="text/markdown",
     author='lxshen',       # 作者
-    author_email='1446036120@qq.com',
+    author_email='guanghui2017@outlook.com',
     maintainer='lxshen',     # 主要的工作人员
-    maintainer_email='1446036120@qq.com',
+    maintainer_email='guanghui2017@outlook.com',
     license='BSD License',
     # packages=['douban'], # 发布的包名
     packages=find_packages(),
